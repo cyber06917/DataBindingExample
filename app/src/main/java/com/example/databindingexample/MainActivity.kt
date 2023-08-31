9575aa6a11feaeb8d0828c85bfaf96a2e4ed3690
cyber <lalitkumarcyber069@gmail.com>
2023-08-31 10:32:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databindingexample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.databindingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
